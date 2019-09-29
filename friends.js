101348a07c69c89a95479bb8b1dfc50f3b082ca4
Tyana Stone <tyanastone@Moniques-MacBook.local>
2019-09-29 04:47:49 -0400

    created sample facts array

---
+++ friends.js
@@ -0,0 +1,11 @@
+var facts = [
+    'In 1993, Matthew Perry co-wrote and pitched a sitcom to NBC about a group of twenty-somethings called Maxwell\'s House the network turned it down as they already had a show in the works that was similar.',
+
+'The show had many names before settling with the name Friends:- Friends Like Us, Six of One, Across the Hall, Once Upon a Time in the West Village and Insomnia Cafe.',
+
+'"Shiny Happy People" by REM was the original choice for the Theme Song for the show and was used in the pilot when the show was still called Friends Like Us.',
+
+'The Producers David Crane and Marta Kauffman co-wrote "I\'ll be there for you" with Phil Solem and Danny Wilde of the Rembrandts. Used for the theme song for all ten series.',
+
+'In 1995, the song reached number 1 in the Billboard Hot 100 Airplay chart for eight weeks.'
+]
